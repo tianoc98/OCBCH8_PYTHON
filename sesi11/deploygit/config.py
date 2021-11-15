1321f48c986530313d577a5f922b0b109186e139
tianoc98 <newakmaret2020@gmail.com>
2021-11-15 12:56:09 +0700

    update

---
+++ sesi11/deploygit/config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mwfjzbzmidmize:434c8e55d52a932c9132f34986e324d6eacc26d4c1f854b77103ef7135e31f58@ec2-18-206-108-36.compute-1.amazonaws.com:5432/d7qdea6n612lfp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
